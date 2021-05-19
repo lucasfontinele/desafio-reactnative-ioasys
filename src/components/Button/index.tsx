98b7d9f7709b93f02611296bb1260d13e2249c2e
Lucas Fontinele <lfontinele07@gmail.com>
2021-05-19 14:44:37 -0300

    Implements Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,23 +1,60 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
+import { useEffect } from 'react';
 
-import { Text, TouchableOpacity } from 'react-native';
+import {TouchableOpacity, ViewStyle, Animated, Easing} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import Icon from 'react-native-vector-icons/FontAwesome';
+import {Container} from './styles';
 
-import { Container } from './styles';
-
-// interface ButtonProps {
-//   children: ReactNode;
-// }
+type ButtonProps = {
+  children: ReactNode;
+  loading?: boolean;
+  onPress: () => void;
+  containerStyle?: ViewStyle;
+  style?: ViewStyle;
+};
 
 Icon.loadFont();
 
-export function Button() {
+export function Button({ children, onPress, style, containerStyle, loading = false }: ButtonProps) {
+  const spinValue = new Animated.Value(0);
+    // Next, interpolate beginning and end values (in this case 0 and 1)
+    const spin = spinValue.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg']
+    });
+
+    // First set up animation
+    const loop = Animated.loop(
+      Animated.timing(
+        spinValue,
+        {
+          toValue: 1,
+          duration: 1000,
+          easing: Easing.linear, // Easing is an additional import from react-native
+          useNativeDriver: true  // To make use of native driver for performance
+        }
+      )
+    );
+
+  useEffect(() => {
+    if (loading) {
+      loop.start();
+    } else {
+      loop.stop();
+    }
+  }, [loading]);
+
   return (
-    <TouchableOpacity onPress={() => {}}>
-      <Container>
-        <Text style={{ fontFamily: 'Poppins-Bold' }}>Button</Text>
-        {/* <Icon name="users" /> */}
+    <TouchableOpacity onPress={onPress} disabled={loading} style={containerStyle}>
+      <Container style={style} loading={loading}>
+        {loading ? (
+          <Animated.View style={{ transform: [{ rotate: spin }] }}>
+            <Icon name="refresh" color="#fff" size={25} />
+          </Animated.View>
+        ): (
+          children
+        )}
       </Container>
     </TouchableOpacity>
   );
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,10 +1,14 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled.View<{ loading: boolean }>`
   width: 100%;
   height: 56px;
   background: #33CC95;
   border-radius: 5px;
   align-items: center;
   justify-content: center;
+
+  ${({ loading }) => loading && css`
+    opacity: 0.5;
+  `}
 `;
